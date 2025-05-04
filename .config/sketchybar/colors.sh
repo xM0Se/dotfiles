5359be1e121c89ac3b7964c8de3eed975adae3af
xM0Se <xM0Se@users.noreply.github.com>
2025-05-04 07:56:56 +0200

    added colors for sketchybar

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export WHITE=0xffffffff
+export BAR_COLOR=0xff1c3d4e
+export ITEM_BG_COLOR=0xff004d6c
+export ACCENT_COLOR=0xff6aa5ee
+export BACKGROUND=0xff00263e
+export GOLD=0xffcc9311
