51a9ae42670d73c779b8a0090573cecda27b2357
xm0se <xm0se>
2025-08-07 12:28:43 +0000

    update

--- nix/pkgs/cli-tools/Shell-Enhancements-UX/module1.nix
+++ nix/pkgs/cli-tools/Shell-Enhancements-UX/module1.nix
@@ -59,7 +59,7 @@
 #
 { nixpkgs, lib, config, ... }: {
 
-  options = {
+  options = { pkgs, ... }: {
     module1.enable = 
       lib.mkEnableOption "enables module1";
   };
@@ -67,7 +67,7 @@
   config = lib.mkIf config.module1.enable {
     environment.systemPackages =
     [
-    pkgs.btop
+    inputs.nixpkgs.btop
     ];
   
   };
