717526a2fe7cf41b02a6c2ad8c1f101ac6e33f23
xm0se <xm0se>
2025-08-07 15:59:32 +0000

    pleaseworknow

--- nix/flake.nix
+++ nix/flake.nix
@@ -193,7 +193,6 @@
        ./pkgs/cli-tools/essential-cli-tools.nix
     ];
     essential-cli-tools.enable = true;
-    eza.enable = true;
     oh-my-posh.enable = true;
 
                         
