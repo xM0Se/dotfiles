27fd2372f27cbb8d25afa8f117718a95cb1baa7b
xm0se <xm0se>
2025-08-08 11:22:19 +0000

    update

--- nix/home-manager/home2.nix
+++ nix/home-manager/home2.nix
@@ -6,6 +6,12 @@
 
   home.stateVersion = "25.05";
 
+  programs.git = {
+    enable = true;
+    userName = "xm0se";
+    userEmail = "189546389+xM0Se@users.noreply.github.com";
+  };
+
   home.packages = [
      pkgs.hello
   ];
