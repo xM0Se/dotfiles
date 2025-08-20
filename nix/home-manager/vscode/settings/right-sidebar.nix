bac5df01872a88e44f6ecd6ef0e843de7dc40ccb
xM0Se <189546389+xM0Se@users.noreply.github.com>
2025-08-20 10:34:10 +0200

    Create right-sidebar.nix

---
+++ nix/home-manager/vscode/settings/right-sidebar.nix
@@ -0,0 +1,14 @@
+{ pkgs, lib, config, ... }: {
+
+  options = {
+    right-sidebar.enable =
+      lib.mkEnableOption "sets the sidebar to the right side";
+  };
+
+  config = lib.mkIf config.right-sidebar.enable {
+    programs.vscode.profiles.default.userSettings = {
+		  "workbench.sideBar.location" = "right";
+    };
+  };
+
+}
