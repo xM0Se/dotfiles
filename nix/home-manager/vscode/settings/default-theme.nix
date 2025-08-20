7921688a37a623c6b8961d74c5f32323a1ea5a94
xM0Se <189546389+xM0Se@users.noreply.github.com>
2025-08-20 09:49:56 +0200

    Update default-theme.nix

--- nix/home-manager/vscode/settings/default-theme.nix
+++ nix/home-manager/vscode/settings/default-theme.nix
@@ -1,1 +1,14 @@
+{ pkgs, lib, config, ... }: {
 
+  options = {
+    default-theme.enable =
+      lib.mkEnableOption "sets the default thme to dracula";
+  };
+
+  config = lib.mkIf config.default-theme.enable {
+    programs.vscode.profiles.default.userSettings = {
+		  "workbench.colorTheme" = "Dracula Theme";
+    };
+  };
+
+}
