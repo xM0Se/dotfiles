b8e1199099a68b6035fcbe619dcbe9afb8fd9f14
xm0se <189546389+xM0Se@users.noreply.github.com>
2025-10-07 20:47:15 +0200

    update

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,1 +1,23 @@
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = augroup("highlight_yank"),
+    callback = function()
+        (vim.hl or vim.highlight).on_yank()
+    end,
+})
 
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then return end
+
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id= client.id })
+                end,
+            })
+        end
+    end,
+})
