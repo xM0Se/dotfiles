db2b8b155abdf770a211f16618d5cccb9efa560a
xM0Se <189546389+xM0Se@users.noreply.github.com>
2025-10-08 11:00:41 +0200

    Update yanky.lua

--- nvim/lua/plugins/yanky.lua
+++ nvim/lua/plugins/yanky.lua
@@ -1,7 +1,11 @@
 return {
   
     "gbprod/yanky.nvim",
-    opts = {},
+    opts = {
+      preserve_cursor_position = {
+        enabled = true,
+    },
+    },
     keys = {
         { "<leader>fy", "<cmd>lua require('telescope').extensions.yank_history.yank_history()<CR>", desc = "Yank History" },
     },
