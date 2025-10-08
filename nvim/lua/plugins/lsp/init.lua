1950b5f661650e15167981c578c57276400d4dd5
xm0se <189546389+xM0Se@users.noreply.github.com>
2025-10-08 17:51:59 +0200

    update

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,14 +1,44 @@
 return {
 
-    "mason-org/mason-lspconfig.nvim",
+    {
+        "mason-org/mason-lspconfig.nvim",
 
-    opts = {
-        ensure_installed = { "lua_ls", "rust_analyzer" },
+        opts = {
+            ensure_installed = { "lua_ls", "rust_analyzer" },
+        },
+
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            {
+                "neovim/nvim-lspconfig",
+
+                config = function()
+                    local capabilites = require("blink.cmp").get_lsp_capabilities()
+                    require("lspconfig").lua_ls.setup({ capabilites = capabilites })
+                end,
+            },
+        },
     },
 
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+        opts = {
+            keymap = { preset = "super-tab" },
 
+            appearance = {
+                nerd_font_variant = "mono",
+            },
+
+            completion = { documentation = { auto_show = false } },
+
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
+
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+        },
+        opts_extend = { "sources.default" },
+    },
 }
