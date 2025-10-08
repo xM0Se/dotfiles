476eb33ed0b7b3c5d96708cba9cf093819b9010d
xm0se <189546389+xM0Se@users.noreply.github.com>
2025-10-08 18:12:32 +0200

    update

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -14,9 +14,7 @@ return {
                 config = function()
                     local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-                    vim.lsp.config["lua_ls"] = { capabilities = capabilities }
-
-                    vim.lsp.enable("lua_ls")
+                    vim.lsp.config = { capabilities = capabilities }
                 end,
             },
         },
