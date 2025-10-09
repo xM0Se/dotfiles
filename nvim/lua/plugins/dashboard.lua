b1df57014efd144cf543a55756362656595291fe
xm0se <189546389+xM0Se@users.noreply.github.com>
2025-10-09 20:46:46 +0200

    update

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -31,6 +31,19 @@ return {
                     key = 'd',
                 },
             },
+            center = {
+                {
+                    icon = ' ',
+                    icon_hl = 'Title',
+                    desc = 'Find File           ',
+                    desc_hl = 'String',
+                    key = 'b',
+                    keymap = 'SPC f f',
+                    key_hl = 'Number',
+                    key_format = ' %s', -- remove default surrounding `[]`
+                    action = 'lua print(2)'
+                },
+            },
         },
     },
 
