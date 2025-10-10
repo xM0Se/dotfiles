eb4c89db36451da4d79b88aa1c87c6fe67776255
xM0Se <189546389+xM0Se@users.noreply.github.com>
2025-10-10 11:00:55 +0200

    Update yanky.lua

--- nvim/lua/plugins/yanky.lua
+++ nvim/lua/plugins/yanky.lua
@@ -2,7 +2,9 @@ return {
   
     "gbprod/yanky.nvim",
     opts = {
-      preserve_cursor_position.enabled = true,
+      preserve_cursor_position = {
+          enabled = true,
+      },
     },
     keys = {
         { "<leader>fy", "<cmd>lua require('telescope').extensions.yank_history.yank_history()<CR>", desc = "Yank History" },
