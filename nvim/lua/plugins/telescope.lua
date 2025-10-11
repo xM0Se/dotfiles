9ad15a0775aa3b02b64fbbc4ab1de189e34b21ae
xm0se <189546389+xM0Se@users.noreply.github.com>
2025-10-11 15:55:30 +0200

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,22 +5,19 @@ return {
         defaults = {
             mappings = {
                 i = {
+                    ["<CR>"] = function(prompt_bufnr)
+                        if require("telescope.actions.state").get_current_line() == ":q" then
+                            require("telescope.actions").close(prompt_bufnr)
+                        else
+                            require("telescope.actions").select_default(prompt_bufnr)
+                        end
+                    end,
                     ["<C-c>"] = require("telescope.actions").close,
                 },
                 n = {
                     ["q"] = require("telescope.actions").close,
                 },
             },
-            attach_mappings = function(prompt_bufnr, _)
-                vim.api.nvim_buf_attach(prompt_bufnr, false, {
-                    on_lines = function()
-                        if require("telescope.actions.state").get_current_line() == ":q" then
-                            require("telescope.actions").close(prompt_bufnr)
-                        end
-                    end,
-                })
-                return true
-            end,
         },
     },
     branch = "master",
