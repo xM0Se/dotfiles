60ff7e7645ae9c7c73607764fc151087a958b8ec
xM0Se <189546389+xM0Se@users.noreply.github.com>
2025-10-30 14:11:21 +0100

    update

--- nix/build.sh
+++ nix/build.sh
@@ -5,5 +5,6 @@ clear
 git commit -m update
 clear
 sudo darwin-rebuild switch --flake ~/dotfiles/nix#dMACOS
+read -p "Press Enter to continue..."
 clear
-ls
+eza --color=always --long --no-time --git --icons=always --no-user --no-permissions --color-scale-mode=gradient --grid --all --group-directories-first
