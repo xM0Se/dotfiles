8f9dd4a5d1174c2821f71ba31abf0f9b9eaa2983
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-01-04 16:49:01 +0100

    working version

--- minecraft-server.nix
+++ minecraft-server.nix
@@ -1,17 +1,20 @@
 {
-	self,
-	pkgs,
-	...
+  self,
+  pkgs,
+  ...
 }: {
+  imports = [
+    ./hosts/nix-os/servers/minecraft-server/configuration.nix
+    ./pkgs/nixpkgs-unstable/cli/u-pkg-essential-cli-tools.nix
+  ];
 
-	imports = [
-		./hosts/nix-os/servers/minecraft-server/configuration.nix
-	];
+  u-pkg-essential-cli-tools.enable = true;
 
-	nixpkgs.hostPlatform = "x86_64-linux";	
-	nixpkgs.config.allowUnfree = true;
-environment.systemPackages = [
-self.packages.${pkgs.system}.nvimconf
-];
-
+  nixpkgs = {
+    hostPlatform = "x86_64-linux";
+    config.allowUnfree = true;
+  };
+  environment.systemPackages = [
+    self.packages.${pkgs.system}.nvimconf
+  ];
 }
