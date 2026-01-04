cac89a67aeb4c8aeb4c5c41d7f1217489dc356e2
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-01-04 18:42:02 +0100

    update

--- configuration/minecraft-servers/default.nix
+++ configuration/minecraft-servers/default.nix
@@ -1,8 +1,4 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{inputs, ...}: {
   imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
 
   nixpkgs.overlays = [inputs.nix-minecraft.overlay];
@@ -12,36 +8,34 @@
     eula = true;
     dataDir = "/root/minecraft-server";
     servers.vanilla = {
-      test = {
-        enable = true;
-        autoStart = true;
-        operators = {
-          xMose = {
-            uuid = "0a9b0753-9941-4861-ab0f-20a82e462ae9";
-            level = 3;
-            bypassesPlayerLimit = true;
-          };
+      enable = true;
+      autoStart = true;
+      operators = {
+        xMose = {
+          uuid = "0a9b0753-9941-4861-ab0f-20a82e462ae9";
+          level = 3;
+          bypassesPlayerLimit = true;
         };
-        serverProperties = {
-          gamemode = "survival";
-          difficulty = "hard";
-          simulation-distance = 10;
-          spawn-protection = 0;
-          hide-online-players = false;
-          log-ips = true;
-          force-gamemode = false;
-          motd = "hope it works";
-          server-port = 25565;
-          view-distance = 10;
-        };
-        whitelist = {
-          xMose = "0a9b0753-9941-4861-ab0f-20a82e462ae9";
-          ElroKnight = "2beb73ed-3cf2-4a4b-a4ae-683db5b71dec";
-          Mathehaeft = "62a27dc6-217f-4c30-b9e8-634f7df68044";
-          Toaster0077282 = "82db6cf8-5a4f-43fe-95f3-486698fd910b";
-        };
-        symlinks = {
-        };
+      };
+      serverProperties = {
+        gamemode = "survival";
+        difficulty = "hard";
+        simulation-distance = 10;
+        spawn-protection = 0;
+        hide-online-players = false;
+        log-ips = true;
+        force-gamemode = false;
+        motd = "hope it works";
+        server-port = 25565;
+        view-distance = 10;
+      };
+      whitelist = {
+        xMose = "0a9b0753-9941-4861-ab0f-20a82e462ae9";
+        ElroKnight = "2beb73ed-3cf2-4a4b-a4ae-683db5b71dec";
+        Mathehaeft = "62a27dc6-217f-4c30-b9e8-634f7df68044";
+        Toaster0077282 = "82db6cf8-5a4f-43fe-95f3-486698fd910b";
+      };
+      symlinks = {
       };
     };
   };
