84be13885e2c14d0db9f832093bc16f250eb7336
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-01-06 19:20:43 +0100

    whitelist

--- configuration/minecraft-servers/default.nix
+++ configuration/minecraft-servers/default.nix
@@ -38,11 +38,11 @@
       whitelist = {
         xMose = "0a9b0753-9941-4861-ab0f-20a82e462ae9";
         ElroKnight = "2beb73ed-3cf2-4a4b-a4ae-683db5b71dec";
-        #        Mathehaeft = "62a27dc6-217f-4c30-b9e8-634f7df68044";
-        #        Toaster0077282 = "82db6cf8-5a4f-43fe-95f3-486698fd910b";
-        #        P1Paxs = "9a3e9ef2-4fdd-4976-9a5c-6986fc7c3a62";
-        #        Muecke1234 = "501c6362-50a6-4438-95e3-23e2e4e63de2";
-        #        GAMERSBLADE5668 = "06264d01-5235-4c5c-8516-a34457b7b723";
+        Mathehaeft = "62a27dc6-217f-4c30-b9e8-634f7df68044";
+        Toaster0077282 = "82db6cf8-5a4f-43fe-95f3-486698fd910b";
+        P1Paxs = "9a3e9ef2-4fdd-4976-9a5c-6986fc7c3a62";
+        Muecke1234 = "501c6362-50a6-4438-95e3-23e2e4e63de2";
+        GAMERSBLADE5668 = "06264d01-5235-4c5c-8516-a34457b7b723";
       };
     };
     # servers.the-new-kingdom = {
