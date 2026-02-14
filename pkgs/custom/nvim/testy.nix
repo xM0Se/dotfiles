288dc1c6d9959562568b0a373f670ddf95860c08
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-02-14 20:55:29 +0100

    put keymaps in diffrent files and added path for undofile

--- pkgs/custom/nvim/testy.nix
+++ pkgs/custom/nvim/testy.nix
@@ -1,99 +1,13 @@
-{pkgs, ...}: {
-  vim = {
-    keymaps = [
-      {
-        key = "J";
-        mode = "v";
-        silent = true;
-        action = ":m '>+1<CR>gv=gv";
-        desc = "";
-      }
-      {
-        key = "K";
-        mode = "v";
-        silent = true;
-        action = ":m '<-2<CR>gv=gv";
-        desc = "";
-      }
-      {
-        key = "<leader>u";
-        mode = "n";
-        silent = true;
-        action = "<cmd>UndotreeToggle<cr>";
-        desc = "toggle undotree";
-      }
-      {
-        key = "<leader>fy";
-        mode = "n";
-        silent = true;
-        action = "<cmd>lua require('telescope').extensions.yank_history.yank_history()<CR>";
-        desc = "Yank History";
-      }
-      {
-        key = "<leader>sv";
-        mode = "n";
-        silent = true;
-        action = "<cmd>vsplit<cr>";
-        desc = "creates vertical split";
-      }
-      {
-        key = "<leader>sh";
-        mode = "n";
-        silent = true;
-        action = "<cmd>split<cr>";
-        desc = "creates split";
-      }
-      {
-        key = "<leader>q";
-        mode = "n";
-        silent = true;
-        action = "<cmd>close<cr>";
-        desc = "close";
-      }
-      {
-        key = "<leader>m";
-        mode = "n";
-        silent = true;
-        action = "<C-w>_<C-w>|";
-        desc = "maximize window";
-      }
-      {
-        key = "<leader>=";
-        mode = "n";
-        silent = true;
-        action = "<C-w>=";
-        desc = "equalize windows";
-      }
-      {
-        key = "<leader>h";
-        mode = "n";
-        silent = true;
-        action = "<cmd>:vertical resize -5<cr>";
-        desc = "resize window -5 verticaly";
-      }
-      {
-        key = "<leader>j";
-        mode = "n";
-        silent = true;
-        action = "<cmd>:resize +5";
-        desc = "resize window +5";
-      }
-      {
-        key = "<leader>k";
-        mode = "n";
-        silent = true;
-        action = "<cmd>:resize -5<cr>";
-        desc = "resize window -5";
-      }
-      {
-        key = "<leader>l";
-        mode = "n";
-        silent = true;
-        action = "<cmd>:vertical resize +5<cr>";
-        desc = "resize window +5 verticaly";
-      }
-    ];
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./keymaps.nix
+  ];
 
+  vim = {
     viAlias = true;
     vimAlias = true;
 
@@ -112,8 +26,10 @@
       mouse = "";
     };
 
-    undoFile.enable = true;
-    # undoFile.path = "../../../.vim/undodir";
+    undoFile = {
+      enable = true;
+      path = inputs.nvf.lib.nvim.lua.mkLuaInline "os.getenv('XDG_DATA_HOME') .. '/nvim/undo'";
+    };
 
     theme = {
       enable = true;
