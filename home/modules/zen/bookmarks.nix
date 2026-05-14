df378fc0425e99856f5e3210daf0fef5eecd7126
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-05-14 10:49:01 +0200

    testing zen

---
+++ home/modules/zen/bookmarks.nix
@@ -0,0 +1,45 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    zen-bookmarks.enable =
+      lib.mkEnableOption "bookmarks";
+  };
+  config = lib.mkIf config.zen-bookmarks.enable {
+    programs.zen-browser.profiles.default.bookmarks = {
+      force = true; # Rewrite bookmarks on each rebuild (overwrite browser changes)
+      settings = [
+        {
+          name = "Nix Sites";
+          toolbar = true;
+          bookmarks = [
+            {
+              name = "homepage";
+              url = "https://nixos.org/";
+            }
+            {
+              name = "wiki";
+              tags = ["wiki" "nix"];
+              url = "https://wiki.nixos.org/";
+            }
+            {
+              name = "packages";
+              url = "https://search.nixos.org/packages";
+            }
+          ];
+        }
+        {
+          name = "Development";
+          bookmarks = [
+            {
+              name = "GitHub";
+              url = "https://github.com";
+            }
+          ];
+        }
+      ];
+    };
+  };
+}
--- home/modules/zen/zenconf.nix
+++ home/modules/zen/zenconf.nix
@@ -23,10 +23,32 @@
   programs.zen-browser = {
     enable = true;
     setAsDefaultBrowser = true;
+
     profiles.default.settings = {
       "zen.workspaces.force-container-workspace" = true; #Switch to workspace where container is set as default when opening container tabs
+      "zen.tabs.vertical" = true;
+      "zen.tabs.vertical.right-side" = true;
+      "zen.welcome-screen.seen" = false;
     };
     policies = {
+      Preferences = {
+        "browser.aboutConfig.showWarning" = {
+          Value = false;
+          Status = "locked";
+        };
+        "browser.tabs.warnOnClose" = {
+          Value = true;
+          Status = "locked"; # User cannot change this
+        };
+        "browser.download.panel.shown" = {
+          Value = true;
+          Status = "locked";
+        };
+        "browser.download.useDownloadDir" = {
+          Value = false;
+          Status = "locked";
+        };
+      };
       AutofillAddressEnabled = true;
       AutofillCreditCardEnabled = false;
       DisableAppUpdate = true;
