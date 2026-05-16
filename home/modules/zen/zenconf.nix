869b6c417816a9421a53d12706549b1c49c252ac
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-05-16 19:51:22 +0200

    test

--- home/modules/zen/zenconf.nix
+++ home/modules/zen/zenconf.nix
@@ -35,7 +35,5 @@
       pkgs.tridactyl-native
     ];
   };
-  home.packages = [
-    pkgs.tridactyl-native
-  ];
+  home.file.".mozilla/native-messaging-hosts/tridactyl.json".source = "${pkgs.tridactyl-native}/lib/mozilla/native-messaging-hosts/tridactyl.json";
 }
