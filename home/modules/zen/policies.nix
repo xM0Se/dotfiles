e1bddc0bed71238fcc0d255e01d0cabcfc6e99ef
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-05-16 14:22:14 +0200

    moved policies from zenconf to seperate file

---
+++ home/modules/zen/policies.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    zen-policies.enable =
+      lib.mkEnableOption "zen-policies";
+  };
+  config = lib.mkIf config.zen-policies.enable {
+    programs.zen-browser.policies.Preferences = {
+      "browser.aboutConfig.showWarning" = {
+        Value = false;
+        Status = "locked";
+      };
+      "browser.tabs.warnOnClose" = {
+        Value = false;
+        Status = "locked"; # User cannot change this
+      };
+      "browser.download.panel.shown" = {
+        Value = true;
+        Status = "locked";
+      };
+      "browser.download.useDownloadDir" = {
+        Value = false;
+        Status = "locked";
+      };
+      "browser.warnOnQuit" = {
+        Value = false;
+        Status = "locked";
+      };
+      "browser.warnOnQuitShortcut" = {
+        Value = false;
+        Status = "locked";
+      };
+      "accessibility.typeaheadfind" = {
+        Value = false;
+        Status = "locked";
+      };
+      # ---------- Configuring Zen Mod "Zen Context Menu (v 3.1)"
+      "widget.macos.native-context-menus" = {
+        Value = false;
+        Status = "locked";
+      };
+      "uc.hidecontext.separators" = {
+        Value = true;
+        Status = "locked";
+      };
+    };
+  };
+}
