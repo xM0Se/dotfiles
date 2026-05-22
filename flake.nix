d5f7b041613ad65287de342bffcf04df07ec5de9
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-05-22 20:58:42 +0200

    updated zen browser input to follow nixpkg and homemanager

--- flake.nix
+++ flake.nix
@@ -30,7 +30,12 @@
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
     };
+
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
