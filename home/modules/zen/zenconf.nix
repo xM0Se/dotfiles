7b1aadc64f93613efb4ef73bc7b7dff5e7e42d00
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-05-22 21:04:17 +0200

    testing tridactly file with homemanager

--- home/modules/zen/zenconf.nix
+++ home/modules/zen/zenconf.nix
@@ -35,5 +35,17 @@
       pkgs.tridactyl-native
     ];
   };
+
+  home.file.".tridactylrc".text = ''
+    set searchurls.g https://www.google.com/search?q=%s
+    set searchurls.ddg https://duckduckgo.com/?q=%s
+    set searchurls.yt https://youtube.com/search?q=%s
+    set searchurls.am https://www.amazon.de/s?k=%s
+    set searchurls.no https://search.nixos.org/options?query=%s
+    set searchurls.np https://search.nixos.org/packages?query=%s
+    set searchurls.gh https://github.com/search?q=%s
+    set searchurls.cve https://nvd.nist.gov/vuln/search/results?query=%s
+  '';
+
   home.file."Library/Application Support/Mozilla/NativeMessagingHosts/tridactyl.json".source = "${pkgs.tridactyl-native}/lib/mozilla/native-messaging-hosts/tridactyl.json";
 }
