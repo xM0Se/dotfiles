3532080777b5f4d3493efc7e6ad0726ed95b4c59
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-05-23 20:40:41 +0200

    always hide workspace nr 1 in sketchybar

--- home/modules/sketchybar/config/plugins/aerospace.sh
+++ home/modules/sketchybar/config/plugins/aerospace.sh
@@ -3,27 +3,32 @@ source "$CONFIG_DIR/colors.sh"
 
 sid="$1"
 
+if [ "$sid" = "1" ]; then
+  sketchybar --set "$NAME" drawing=off
+  exit 0
+fi
+
 win_count=$(aerospace list-windows --workspace "$sid" 2>/dev/null | wc -l | tr -d ' ')
 
 if [ "$sid" = "$FOCUSED_WORKSPACE" ]; then
-    LABEL_COLOR="$OPEN_SPACE_COLOR"
+  LABEL_COLOR="$OPEN_SPACE_COLOR"
 else
-    LABEL_COLOR="$TEXT"
+  LABEL_COLOR="$TEXT"
 fi
 
 if [ "$sid" = "$FOCUSED_WORKSPACE" ] || [ "$win_count" -gt 0 ]; then
 
-    sketchybar --set "$NAME" \
-        label="$sid" \
-        label.color="$LABEL_COLOR" \
-        label.font.size=15 \
-        label.align=center \
-        icon.drawing=off \
-        background.drawing=off \
-        drawing=on
+  sketchybar --set "$NAME" \
+    label="$sid" \
+    label.color="$LABEL_COLOR" \
+    label.font.size=15 \
+    label.align=center \
+    icon.drawing=off \
+    background.drawing=off \
+    drawing=on
 
 else
 
-    sketchybar --set "$NAME" drawing=off
+  sketchybar --set "$NAME" drawing=off
 
 fi
