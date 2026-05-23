0bc542a5ce96349f2ea17f7606a332a6cca93046
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-05-23 21:18:50 +0200

    added ghostty config

---
+++ home/modules/ghostty/default.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.ghostty-bin;
+    enableZshIntegration = true;
+    settings = {
+      mouse-hide-while-typing = true;
+      copy-on-select = true;
+      keybind = [
+        "global:cmd+t+shift=toggle_quick_terminal"
+        "keybind = option+backspace=text:\x17"
+      ];
+      theme = "Rose Pine Moon";
+      macos-titlebar-style = "hidden";
+    };
+  };
+}
