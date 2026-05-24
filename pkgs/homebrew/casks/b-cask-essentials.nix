e347bc10082710e402f3ad2a42a8aca84a17745c
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-05-24 22:14:33 +0200

    removed affinity from brew packages since no longer in use

--- pkgs/homebrew/casks/b-cask-essentials.nix
+++ pkgs/homebrew/casks/b-cask-essentials.nix
@@ -11,7 +11,6 @@
     ./b-cask-homerow.nix
     ./b-cask-kindavim.nix
     ./b-cask-lulu.nix
-    ./b-cask-affinity.nix
     ./b-cask-beeper.nix
     ./b-cask-hammerspoon.nix
     ./b-cask-blackhole.nix
@@ -36,8 +35,6 @@
     b-cask-kindavim.enable =
       lib.mkDefault true;
     b-cask-lulu.enable =
-      lib.mkDefault true;
-    b-cask-affinity.enable =
       lib.mkDefault true;
     b-cask-beeper.enable =
       lib.mkDefault true;
