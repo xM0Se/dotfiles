235d39ca934401bc96316816588bd162c3d105e7
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-05-30 21:58:10 +0200

    added player to whitelist and temp moderator

--- configuration/minecraft-servers/default.nix
+++ configuration/minecraft-servers/default.nix
@@ -29,6 +29,9 @@
         server-port = 25565;
         view-distance = 9;
       };
+      operators = {
+        ElroKnight = "2beb73ed-3cf2-4a4b-a4ae-683db5b71dec";
+      };
       whitelist = {
         xMose = "0a9b0753-9941-4861-ab0f-20a82e462ae9";
         ElroKnight = "2beb73ed-3cf2-4a4b-a4ae-683db5b71dec";
@@ -39,6 +42,7 @@
         GAMERSBLADE5668 = "06264d01-5235-4c5c-8516-a34457b7b723";
         Itz_mikalli = "c50d4b30-2e56-4f03-84da-ce195fcefaee";
         Jari2k24 = "7bb0ef18-eb20-4a80-a3f8-8faf652307d9";
+        Jonulinka = "5f0eacfc-6d33-4778-8ae9-37a738cd2cf2";
       };
       symlinks = {
         mods = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
