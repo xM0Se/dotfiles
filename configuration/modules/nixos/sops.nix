1192a47f96fc679364fce63c92331f77791a268d
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-06-13 23:08:03 +0200

    update

--- configuration/modules/nixos/sops.nix
+++ configuration/modules/nixos/sops.nix
@@ -7,7 +7,7 @@
   sops = {
     defaultSopsFile = "${self}/secrets/secrets.yaml";
     defaultSopsFormat = "yaml";
-    sshKeyPaths = [];
+    gnupg.sshKeyPaths = [];
     age.keyFile = "/etc/sops.age.key";
   };
 }
