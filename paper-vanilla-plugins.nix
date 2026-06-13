376b7847192703281a343aef0900c1a3a64475e8
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-06-13 22:08:39 +0200

    removed wrong placed file

--- paper-vanilla-plugins.nix
+++
@@ -1,4 +0,0 @@
-# auto generated with nix-minecraft-plugin-upgrade --loader purpur --game-version 26.1.2 --project teaks-tweaks
-{ pkgs, ... }: {
-  "plugins/teakstweaks-2.1.3.jar" = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/Xdn5t532/versions/MxwUtmy2/teakstweaks-2.1.3.jar"; sha512 = "08c33637d1983931ffd2b36d3c88971077daa8865facc8a00220761ad35552f195a27ccacffd132f2bd44034020542e166bdc1f96f487260bba8a4a2fac57080"; };
-}
