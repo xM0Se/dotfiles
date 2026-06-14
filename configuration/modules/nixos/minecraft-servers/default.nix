ee0b9f6793da9fe25aca89c0fb90dd1e008212af
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-06-14 10:09:29 +0200

    moved user whitelist for mincraft server to a separate file

--- configuration/modules/nixos/minecraft-servers/default.nix
+++ configuration/modules/nixos/minecraft-servers/default.nix
@@ -34,19 +34,7 @@
         server-port = 25565;
         view-distance = 15;
       };
-      whitelist = {
-        xMose = "0a9b0753-9941-4861-ab0f-20a82e462ae9";
-        ElroKnight = "2beb73ed-3cf2-4a4b-a4ae-683db5b71dec";
-        Mathehaeft = "62a27dc6-217f-4c30-b9e8-634f7df68044";
-        Toaster0077282 = "82db6cf8-5a4f-43fe-95f3-486698fd910b";
-        P1Paxs = "9a3e9ef2-4fdd-4976-9a5c-6986fc7c3a62";
-        Muecke1234 = "501c6362-50a6-4438-95e3-23e2e4e63de2";
-        GAMERSBLADE5668 = "06264d01-5235-4c5c-8516-a34457b7b723";
-        Itz_mikalli = "c50d4b30-2e56-4f03-84da-ce195fcefaee";
-        Jari2k24 = "7bb0ef18-eb20-4a80-a3f8-8faf652307d9";
-        Jonulinka = "5f0eacfc-6d33-4778-8ae9-37a738cd2cf2";
-        Fynndus135 = "52236dbe-88b6-4c28-8e0a-ae0d71c61a2c";
-      };
+      whitelist = import ./whitelist.nix;
       symlinks =
         {
           "plugins/TeaksTweaks/config.yml" = pkgs.writeTextFile {
