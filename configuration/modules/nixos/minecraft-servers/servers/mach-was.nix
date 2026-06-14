8239509e2425f87509f4bdcf4c19ee13830a78ee
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-06-14 11:41:06 +0200

    missed closing bracket

--- configuration/modules/nixos/minecraft-servers/servers/mach-was.nix
+++ configuration/modules/nixos/minecraft-servers/servers/mach-was.nix
@@ -23,27 +23,27 @@
       }
       // (import ../plugins/purpur/plugins.nix {inherit pkgs;})
       // (import ../plugins/paper/plugins.nix {inherit pkgs;});
-  };
-  files = {
-    "config/paper-world-defaults.yml" = pkgs.writeTextFile {
-      name = "paper-world-defaults.yml";
-      text = builtins.readFile ../config/purpur/paper/paper-world-defaults.yml;
-    };
-    "config/paper-global.yml" = pkgs.writeTextFile {
-      name = "paper-global.yml";
-      text = builtins.readFile ../config/purpur/paper/paper-global.yml;
-    };
-    "purpur.yml" = pkgs.writeTextFile {
-      name = "purpur.yml";
-      text = builtins.readFile ../config/purpur/purpur.yml;
-    };
-    "spigot.yml" = pkgs.writeTextFile {
-      name = "spigot.yml";
-      text = builtins.readFile ../config/purpur/spigot.yml;
-    };
-    "bukkit.yml" = pkgs.writeTextFile {
-      name = "bukkit.yml";
-      text = builtins.readFile ../config/purpur/bukkit.yml;
+    files = {
+      "config/paper-world-defaults.yml" = pkgs.writeTextFile {
+        name = "paper-world-defaults.yml";
+        text = builtins.readFile ../config/purpur/paper/paper-world-defaults.yml;
+      };
+      "config/paper-global.yml" = pkgs.writeTextFile {
+        name = "paper-global.yml";
+        text = builtins.readFile ../config/purpur/paper/paper-global.yml;
+      };
+      "purpur.yml" = pkgs.writeTextFile {
+        name = "purpur.yml";
+        text = builtins.readFile ../config/purpur/purpur.yml;
+      };
+      "spigot.yml" = pkgs.writeTextFile {
+        name = "spigot.yml";
+        text = builtins.readFile ../config/purpur/spigot.yml;
+      };
+      "bukkit.yml" = pkgs.writeTextFile {
+        name = "bukkit.yml";
+        text = builtins.readFile ../config/purpur/bukkit.yml;
+      };
     };
   };
 }
