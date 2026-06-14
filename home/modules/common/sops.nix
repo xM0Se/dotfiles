961927d78d4311dd80e8bb04f07c31a22c1b9348
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-06-14 16:36:09 +0200

    updated path to be self

--- home/modules/common/sops.nix
+++ home/modules/common/sops.nix
@@ -1,11 +1,12 @@
 {
   inputs,
+  self,
   config,
   ...
 }: {
   imports = [inputs.sops-nix.homeManagerModules.sops];
   sops = {
-    defaultSopsFile = ../../../secrets/secrets.yaml;
+    defaultSopsFile = "${self}/secrets/secrets.yaml";
     defaultSopsFormat = "yaml";
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
   };
