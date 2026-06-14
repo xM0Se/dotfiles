9860266df01807aeaa3cc2bb12e8ec95e25ad891
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-06-14 19:10:27 +0200

    added backup plugin to server

--- configuration/modules/nixos/minecraft-servers/plugins/helper.sh
+++ configuration/modules/nixos/minecraft-servers/plugins/helper.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 LATEST_VERSION=$(curl -s https://api.purpurmc.org/v2/purpur | jq -r '.versions[-1]')
 
+# ---purpur
 nix run github:BatteredBunny/nix-minecraft-plugin-upgrade -- \
   --loader purpur \
   --game-version "$LATEST_VERSION" \
   --project teaks-tweaks \
   >./purpur/teaks-tweaks.nix
 
+nix run github:BatteredBunny/nix-minecraft-plugin-upgrade -- \
+  --loader purpur \
+  --game-version "$LATEST_VERSION" \
+  --project drivebackupv2 \
+  >./purpur/drivebackupv2.nix
+
+# ---paper
 nix run github:BatteredBunny/nix-minecraft-plugin-upgrade -- \
   --loader paper \
   --game-version "$LATEST_VERSION" \
---
+++ configuration/modules/nixos/minecraft-servers/plugins/purpur/drivebackupv2.nix
@@ -0,0 +1,4 @@
+# auto generated with nix-minecraft-plugin-upgrade --loader purpur --game-version 26.1.2 --project drivebackupv2
+{ pkgs, ... }: {
+  "plugins/DriveBackupV2.jar" = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/BQbKkmMm/versions/ACUmNLdS/DriveBackupV2.jar"; sha512 = "479442c5609a5fa309910722b55e12a212aa3d44a453f753d9295e14a9bfbd95cbf4320f37f9e3e308195437e1fb11d15b773dc0e2fbf4a45b68ee4121d983fb"; };
+}
