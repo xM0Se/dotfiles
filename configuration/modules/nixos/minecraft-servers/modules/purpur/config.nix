603f09e866fea4ac87abae89279068970f83ca98
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-06-14 18:39:36 +0200

    fixed paths

--- configuration/modules/nixos/minecraft-servers/modules/purpur/config.nix
+++ configuration/modules/nixos/minecraft-servers/modules/purpur/config.nix
@@ -1,22 +1,22 @@
 {pkgs, ...}: {
   "config/paper-world-defaults.yml" = pkgs.writeTextFile {
     name = "paper-world-defaults.yml";
-    text = builtins.readFile ../config/purpur/paper/paper-world-defaults.yml;
+    text = builtins.readFile ../../config/purpur/paper/paper-world-defaults.yml;
   };
   "config/paper-global.yml" = pkgs.writeTextFile {
     name = "paper-global.yml";
-    text = builtins.readFile ../config/purpur/paper/paper-global.yml;
+    text = builtins.readFile ../../config/purpur/paper/paper-global.yml;
   };
   "purpur.yml" = pkgs.writeTextFile {
     name = "purpur.yml";
-    text = builtins.readFile ../config/purpur/purpur.yml;
+    text = builtins.readFile ../../config/purpur/purpur.yml;
   };
   "spigot.yml" = pkgs.writeTextFile {
     name = "spigot.yml";
-    text = builtins.readFile ../config/purpur/spigot.yml;
+    text = builtins.readFile ../../config/purpur/spigot.yml;
   };
   "bukkit.yml" = pkgs.writeTextFile {
     name = "bukkit.yml";
-    text = builtins.readFile ../config/purpur/bukkit.yml;
+    text = builtins.readFile ../../config/purpur/bukkit.yml;
   };
 }
