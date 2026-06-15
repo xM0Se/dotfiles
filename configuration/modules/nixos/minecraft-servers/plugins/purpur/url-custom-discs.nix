813247b0cfccab7fcfd22e1a7df08906c45334da
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-06-15 21:21:32 +0200

    chore(minecraft-servers): updated plugins

---
+++ configuration/modules/nixos/minecraft-servers/plugins/purpur/url-custom-discs.nix
@@ -0,0 +1,4 @@
+# auto generated with nix-minecraft-plugin-upgrade --loader purpur --game-version 26.1.2 --project url-custom-discs
+{ pkgs, ... }: {
+  "plugins/URLCustomDiscs-v2.7.2-1.21-1.21.8.jar" = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/9dkRl54Z/versions/OOVxcdpM/URLCustomDiscs-v2.7.2-1.21-1.21.8.jar"; sha512 = "48f77b9e16e5fc1e47f96c14e75d9264ca0f8e2f3e7a26cda84c9fed2fb6a225543cbca0c21936170a776e39cfe2dbfaf85bd33f30102175c4a13d555d410f3c"; };
+}
