7bd6fb9bd7545f40ee0d49f8e58c70ab79e7d98e
xm0se <189546389+xM0Se@users.noreply.github.com>
2026-07-01 17:13:15 +0200

    feat(home/vesktop): removed vunarable pnpm version

--- home/modules/common/vesktop/default.nix
+++ home/modules/common/vesktop/default.nix
@@ -1,6 +1,14 @@
-_: {
+{pkgs, ...}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      vesktop = prev.vesktop.override {
+        pnpm = final.pnpm_10;
+      };
+    })
+  ];
   programs.vesktop = {
     enable = true;
+    package = pkgs.vesktop;
     vencord = {
       themes = {rose-pine-moon = ./rose-pine-moon.css;};
       settings = {
